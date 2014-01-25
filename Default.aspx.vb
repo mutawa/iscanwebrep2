0cb255d5dcf2e9582fdc0adb56393bdefe06aa04
mutawa <ahmad.mutawa@gmail.com>
2014-01-26 02:34:36 +0300

    Second attempt

---
+++ Default.aspx.vb
@@ -0,0 +1,10 @@
+﻿Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Me.GridView2.DataSource = getTable("select count(*) from users")
+        Me.GridView2.DataBind()
+
+    End Sub
+
+End Class
